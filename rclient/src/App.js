ef89cf01563399a74691b1ad08e0d32536f6178e
natheraf <ma.eric11214@outlook.com>
2025-10-05 21:06:42 -0400

    fix: remove commented container

--- rclient/src/App.js
+++ rclient/src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <Router>
       <Header />
-      {/* <Container maxWidth="xl" sx={{ mt: "88px" }}></Container> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<h1>Wrong path</h1>} />
